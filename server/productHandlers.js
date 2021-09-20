e8135985bbacedcd239ec17dbcb57842c6dd2408
bcha92@hotmail.com <bcha92@hotmail.com>
2021-09-20 13:46:03 -0400

    create-backend

---
+++ server/productHandlers.js
@@ -0,0 +1,10 @@
+// Middleware/Backend Product Handlers
+
+// GET a list of all products
+const getProducts = async (req, res) => {};
+
+// GET a product by product ID
+const getProductById = async (req, res) => {};
+
+// Module Exports
+module.exports = { getProducts, getProductById };
