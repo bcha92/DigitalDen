42a2d24a41aa1f201fa784173052ab72f1dcbb26
Piyush <piyushpatel.cloud@yahoo.com>
2021-09-20 15:50:59 -0400

    layout update

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,14 +1,17 @@
 import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
-import Header from "./Header";
-import Footer from "./Footer";
-import Brands from "./Brands";
-import AllProducts from "./AllProducts";
+import { Header } from "./Header";
+import { Footer } from "./Footer";
+import { Brands } from "./Brands";
+import { Homepage } from "./Homepage";
+import { AllProducts } from "./AllProducts";
+import { ProductDetails } from "./ProductDetails";
+import { Category } from "./Category";
+import { Checkout } from "./Checkout";
 
 const App = () => {
   return (
     <BrowserRouter>
-      <GlobalStyles />
       <Header />
       <Switch>
         <Route exact path="/">
@@ -18,7 +21,7 @@ const App = () => {
           <Brands />
         </Route>
         <Route exact path="/details/:_id">
-          <ProductsDetails />
+          <ProductDetails />
         </Route>
         <Route exact path="/products">
           <AllProducts />
