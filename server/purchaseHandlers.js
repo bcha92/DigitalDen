bb555b33871f2167300e522719bf098b69c0d08e
bcha92@hotmail.com <bcha92@hotmail.com>
2021-09-23 11:53:52 -0400

    updated backend--PATCH for checkout page

--- server/purchaseHandlers.js
+++ server/purchaseHandlers.js
@@ -1,46 +1,77 @@
+// Parses JSON files
+const fs = require("fs");
+const items = JSON.parse(fs.readFileSync("data/items.json"));
+
+// Error Handler
+const { Error404 } = require("./ErrorHandler");
+
 // Inventory Check Middleware
 const inventoryCheck = async (req, res, next) => {
+    // req.body in this case should be an array from localStorage Cart
+    const cart = req.body;
+
+    try {
+        let inventoryArray = [];
+        let isInventoryMismatched = false;
+        // Iterates through cart to ensure inventory has enough in stock to process transaction
+        cart.forEach(cartItem => {
+            items.forEach(inventory => {
+                if (cartItem._id === inventory._id) {
+                    if (cartItem.quantity > inventory.numInStock) {
+                        isInventoryMismatched = true;
+                    }
+                    inventoryArray.push({
+                        _id: inventory._id,
+                        name: inventory.name,
+                        quantity: cartItem.quantity,
+                        numInStock: inventory.numInStock,
+                    });
+                }
+            })
+        })
+        // Returns an error if indicated quantity exceeds number of items in stock.
+        if (isInventoryMismatched) {
+            return res.status(400).json({
+                status: 400,
+                message: "Error! One or more items in your cart has more quantity than the available number of items in stock.",
+                data: inventoryArray,
+            })
+        }
+    }
+    catch (err) {
+        console.log("inventoryCheck Error:", err);
+    }
     next();
     // Once all checks are passed, middleware is complete and moves on to the "next" handler: purchaseHandle
 };
 
 // Purchase Handle
 const purchaseHandle = async (req, res) => {
-    const { _id } = req.params; // Product ID Here
+    // req.body in this case should be an array from localStorage Cart
+    const cart = req.body;
 
     try {
-        // New Variable to Filter Product
-        let product;
-        // forEach iterates through each product
-        // If product _id matches req.params, assign product to the variable "product"
-        items.forEach(item => {
-            if (item._id == _id) {
-                product = item;
-            }
+        let inventoryArray = [];
+        // forEach iterates through each item in cart
+        cart.forEach(cartItem => {
+            // forEach iterates through each item in inventory for the cartItem
+            items.forEach(inventory => {
+                // Once cartItem has been found in inventory, inventory numInStock is updated based on the purchase quantity of each cartItem
+                if (cartItem._id === inventory._id) {
+                    inventory.numInStock -= cartItem.quantity;
+                    inventoryArray.push(inventory);
+                }
+            })
         });
 
-        // Return an error message if no such product is found.
-        if (product === undefined) {
-            return res.status(404).json(Error404);
-        }
-        // Return error if product has no quantity in stock
-        else if (product.numInStock === 0) {
-            return res.status(400).json({
-                status: 400,
-                message: "Sorry! This product is sold out. Please check again later.",
-                data: product,
-            })
-        }
-        else { // Decrement quantity in stock and confirm purchase of item.
-            product.numInStock--;
-            return res.status(200).json({
-                status: 204,
-                message: `Thank you for purchasing ${product.name}. Your order is now processing.`,
-                data: product,
-            })
-        }
+        // Once inventory is updated, a thank you message is returned. **IMPORTANT: AS LOCALSTORAGE IS A FRONT-END OBJECT, REMOVING THE ITEMS FROM LOCAL STORAGE IS IMPORTANT, BUT MUST BE DONE ON FRONT-END AFTER THIS FETCH REQUEST IS COMPLETED!!!**
+        return res.status(200).json({
+            status: 204,
+            message: `Thank you for your purchase. Your order is now sent for processing.`,
+            data: inventoryArray,
+        })
     }
-    catch (err) {
+    catch (err) { // Error Catch
         console.log("updateProductPurchase Error:", err);
     }
 };
