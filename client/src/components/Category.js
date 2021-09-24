d0df8026b41ccd6325457e6cb6cc87da4c37a095
Ned Marafawi <70671308+nedmarafawi@users.noreply.github.com>
2021-09-23 20:20:05 -0400

    Add images to Category and hover animation

--- client/src/components/Category.js
+++ client/src/components/Category.js
@@ -2,6 +2,14 @@ import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 import { Link } from "react-router-dom";
 
+import fitnessImg from "./assets/fitness.jpeg";
+import medicalImg from "./assets/medical.jpeg";
+import lifestyleImg from "./assets/lifestyle.jpg";
+import entertainmentImg from "./assets/entertainment.jpeg";
+import industrialImg from "./assets/industrial.jpeg";
+import animalImg from "./assets/animal.jpg";
+import gamingImg from "./assets/gaming.jpg";
+
 export const Category = () => {
   //this will store all category from fetch
   const [category, setCategory] = useState();
@@ -23,9 +31,6 @@ export const Category = () => {
 
   return (
     <Wrapper>
-      <Title>
-        <h1>Shop By Category</h1>
-      </Title>
       {isLoaded ? (
         <>
           <Container>
@@ -35,9 +40,7 @@ export const Category = () => {
                   to={`/category/${name}`}
                   style={{ textDecoration: "none" }}
                   key={name}
-                >
-                  <Card>{name}</Card>
-                </Link>
+                ></Link>
               );
             })}
           </Container>
@@ -45,6 +48,99 @@ export const Category = () => {
       ) : (
         <h1>Loading...</h1>
       )}
+      <CardWrapper>
+        <Link to="/category/Fitness">
+          <CategoryFitness
+            style={{
+              width: "350px",
+              height: "350px",
+              backgroundImage: `url(${fitnessImg})`,
+              backgroundSize: "cover",
+              backgroundPosition: "center",
+            }}
+          >
+            <Text>Fitness</Text>
+          </CategoryFitness>
+        </Link>
+        <Link to="/category/Medical">
+          <CategoryMedical
+            style={{
+              width: "350px",
+              height: "350px",
+              backgroundImage: `url(${medicalImg})`,
+              backgroundSize: "cover",
+              backgroundPosition: "center",
+            }}
+          >
+            <Text>Medical</Text>
+          </CategoryMedical>
+        </Link>
+        <Link to="/category/Lifestyle">
+          <CategoryLifestyle
+            style={{
+              width: "350px",
+              height: "350px",
+              backgroundImage: `url(${lifestyleImg})`,
+              backgroundSize: "cover",
+              backgroundPosition: "center",
+            }}
+          >
+            <Text>Lifestyle</Text>
+          </CategoryLifestyle>
+        </Link>
+        <Link to="/category/Entertainment">
+          <CategoryEntertainment
+            style={{
+              width: "350px",
+              height: "350px",
+              backgroundImage: `url(${entertainmentImg})`,
+              backgroundSize: "cover",
+              backgroundPosition: "center",
+            }}
+          >
+            <Text>Entertainment</Text>
+          </CategoryEntertainment>
+        </Link>
+        <Link to="/category/Industrial">
+          <CategoryIndustrial
+            style={{
+              width: "350px",
+              height: "350px",
+              backgroundImage: `url(${industrialImg})`,
+              backgroundSize: "cover",
+              backgroundPosition: "center",
+            }}
+          >
+            <Text>Industrial</Text>
+          </CategoryIndustrial>
+        </Link>
+        <Link to="/category/Pets and Animals">
+          <CategoryPetsAnimal
+            style={{
+              width: "350px",
+              height: "350px",
+              backgroundImage: `url(${animalImg})`,
+              backgroundSize: "cover",
+              backgroundPosition: "center",
+            }}
+          >
+            <Text>Pets and Animals</Text>
+          </CategoryPetsAnimal>
+        </Link>
+        <Link to="/category/Gaming">
+          <CategoryGaming
+            style={{
+              width: "350px",
+              height: "350px",
+              backgroundImage: `url(${gamingImg})`,
+              backgroundSize: "cover",
+              backgroundPosition: "center",
+            }}
+          >
+            <Text class="card__background">Gaming</Text>
+          </CategoryGaming>
+        </Link>
+      </CardWrapper>
     </Wrapper>
   );
 };
@@ -54,32 +150,175 @@ const Wrapper = styled.div`
   width: 100vw;
   font-family: "Oswald", sans-serif;
 `;
-const Title = styled.div`
-  display: flex;
-  justify-content: center;
-  margin: 50px 0;
-  font-size: 25px;
-`;
+
 const Container = styled.div`
   display: flex;
   flex-wrap: wrap;
   justify-content: center;
   align-items: center;
 `;
-const Card = styled.div`
-  background-color: white;
-  height: 50px;
-  width: 200px;
-  font-size: 15px;
+
+const CardWrapper = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr 2fr;
+  grid-template-rows: repeat(2, 21vw);
+  justify-content: center;
+  grid-gap: 1px;
+  margin-left: 20.5rem;
+`;
+
+const CategoryFitness = styled.div`
+  border-radius: 7px;
   display: flex;
-  flex-direction: column;
   align-items: center;
   justify-content: center;
-  border: 1px solid white;
-  padding: 10px;
-  margin: 10px;
-  box-shadow: 5px 5px 4px #888888;
-  border-radius: 20px;
-  color: black;
-  font-weight: bold;
+  transition: 0.3s ease-out;
+  margin: 50px 25px;
+  font-size: 25px;
+
+  &:hover {
+    transform: translateY(-5px) scale(1.005) translateZ(0);
+    box-shadow: 0 24px 36px rgba(0, 0, 0, 0.11), 0 24px 46px #c4c4c4;
+  }
+  &:active {
+    transform: scale(1) translateZ(0);
+    box-shadow: 0 15px 24px rgba(0, 0, 0, 0.11), 0 15px 24px #c4c4c4;
+  }
 `;
+
+const CategoryMedical = styled.div`
+  border-radius: 7px;
+  width: 350px;
+  height: 350px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  transition: 0.3s ease-out;
+  margin: 50px 25px;
+  font-size: 25px;
+
+  &:hover {
+    transform: translateY(-5px) scale(1.005) translateZ(0);
+    box-shadow: 0 24px 36px rgba(0, 0, 0, 0.11), 0 24px 46px #c4c4c4;
+  }
+  &:active {
+    transform: scale(1) translateZ(0);
+    box-shadow: 0 15px 24px rgba(0, 0, 0, 0.11), 0 15px 24px #c4c4c4;
+  }
+`;
+const CategoryLifestyle = styled.div`
+  border-radius: 7px;
+  width: 350px;
+  height: 350px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  transition: 0.3s ease-out;
+  margin: 50px 25px;
+  font-size: 25px;
+
+  &:hover {
+    transform: translateY(-5px) scale(1.005) translateZ(0);
+    box-shadow: 0 24px 36px rgba(0, 0, 0, 0.11), 0 24px 46px #c4c4c4;
+  }
+  &:active {
+    transform: scale(1) translateZ(0);
+    box-shadow: 0 15px 24px rgba(0, 0, 0, 0.11), 0 15px 24px #c4c4c4;
+  }
+`;
+
+const CategoryEntertainment = styled.div`
+  border-radius: 7px;
+  width: 350px;
+  height: 350px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  transition: 0.3s ease-out;
+  margin: 50px 25px;
+  font-size: 25px;
+
+  &:hover {
+    transform: translateY(-5px) scale(1.005) translateZ(0);
+    box-shadow: 0 24px 36px rgba(0, 0, 0, 0.11), 0 24px 46px #c4c4c4;
+  }
+  &:active {
+    transform: scale(1) translateZ(0);
+    box-shadow: 0 15px 24px rgba(0, 0, 0, 0.11), 0 15px 24px #c4c4c4;
+  }
+`;
+
+const CategoryIndustrial = styled.div`
+  border-radius: 7px;
+  width: 350px;
+  height: 350px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  transition: 0.3s ease-out;
+  margin: 50px 25px;
+  font-size: 25px;
+
+  &:hover {
+    transform: translateY(-5px) scale(1.005) translateZ(0);
+    box-shadow: 0 24px 36px rgba(0, 0, 0, 0.11), 0 24px 46px #c4c4c4;
+  }
+  &:active {
+    transform: scale(1) translateZ(0);
+    box-shadow: 0 15px 24px rgba(0, 0, 0, 0.11), 0 15px 24px #c4c4c4;
+  }
+`;
+
+const CategoryPetsAnimal = styled.div`
+  border-radius: 7px;
+  width: 350px;
+  height: 350px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  transition: 0.3s ease-out;
+  margin: 50px 25px;
+  font-size: 25px;
+
+  &:hover {
+    transform: translateY(-5px) scale(1.005) translateZ(0);
+    box-shadow: 0 24px 36px rgba(0, 0, 0, 0.11), 0 24px 46px #c4c4c4;
+  }
+  &:active {
+    transform: scale(1) translateZ(0);
+    box-shadow: 0 15px 24px rgba(0, 0, 0, 0.11), 0 15px 24px #c4c4c4;
+  }
+`;
+
+const CategoryGaming = styled.div`
+  border-radius: 7px;
+  width: 350px;
+  height: 350px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  transition: 0.3s ease-out;
+  margin: 50px 25px;
+  font-size: 25px;
+
+  &:hover {
+    transform: translateY(-5px) scale(1.005) translateZ(0);
+    box-shadow: 0 24px 36px rgba(0, 0, 0, 0.11), 0 24px 46px #c4c4c4;
+  }
+  &:active {
+    transform: scale(1) translateZ(0);
+    box-shadow: 0 15px 24px rgba(0, 0, 0, 0.11), 0 15px 24px #c4c4c4;
+    filter: brightness(0.5) saturate(0) contrast(1.2) blur(20px);
+  }
+`;
+
+const Text = styled.p`
+  font-weight: 500;
+  text-decoration: none;
+  color: white;
+  text-shadow: 2px 2px 4px #000000;
+  font-family: var(--heading-font-family);
+  font-weight: 700;
+`;
+
+const Img = styled.img``;
